aef5cb398a9080e805b000efbb8abb43cd255e20
mouhamed2213 <Mouhamedbk1002@gmail.com>
2026-08-27 22:45:47 +0000

    refactor: remove legacy notification controller

--- src/controllers/notification.controller.ts
+++
@@ -1,6 +0,0 @@
-import { NotificationController as DomainNotificationController } from "../modules/notification/notification.controller.js";
-import { NotificationService } from "../modules/notification/notification.service.js";
-
-export const sendNotificationToShop = NotificationService.sendToShop;
-export const streamNotifications = DomainNotificationController.streamNotifications;
-export const getStockAlerts = DomainNotificationController.getStockAlerts;
